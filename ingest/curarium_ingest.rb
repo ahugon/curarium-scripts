3a23d83de0fdc07a52ad69c2fa3365129ef5db38
Alex Hugon <ahugon@post.harvard.edu>
2014-01-17 08:00:47 -0500

    added usage and tweaks to curarium_ingest script

--- ingest/curarium_ingest.rb
+++ ingest/curarium_ingest.rb
@@ -2,11 +2,15 @@ class Curarium
 	require 'net/http'
 	require 'uri'
 	
+	# USAGE: ruby -r "./curarium_ingest.rb" -e "Curarium.ingest('PATH_TO_DIRECTORY', 'COLLECTION_INGEST_KEY')"
+	# EXAMPLE, if running from ingest/ directory: 
+	#          ruby -r "./curarium_ingest.rb" -e "Curarium.ingest('../output/json_output', 'COLLECTION_INGEST_KEY')"
+	
 	def self.ingest(path, key)
 		
 		# constant for URL
-		base = "http://localhost:3000"
-		#base = "http://www.curarium.com"
+		base = "http://localhost:3000" #for ingesting to local site
+		#base = "http://www.curarium.com" #for ingesting to live site
 	
 		print "Verifying that directory './#{path}' exists... "
 		begin
@@ -24,8 +28,7 @@ class Curarium
 		
 		# check whether key is the key of any existing collection
 		response = Net::HTTP.post_form(URI.parse(base + "/collections/" + key + "/check_key"), {})
-		#puts ""
-		#puts response.body
+
 		if response.body.to_s.index("true").nil?
 			puts ""
 			puts "ERROR: key '#{key}' does not correspond to any existing Curarium collection!"
@@ -37,7 +40,7 @@ class Curarium
 		ent.each do |f| 
 			if !File.directory?(f) and File.extname(f).casecmp(".json").zero?
 				filename = "./#{path}/#{File.basename(f)}"
-				print "Processing: #{filename}... "
+				print "Processing #{(j_count + 1).to_s} of #{ent.count - 2}: #{filename}... "
 				
 				# extract json of this file's record
 				rec_json = IO.read(filename)
@@ -45,10 +48,9 @@ class Curarium
 				# send the record's json to server
 				response = Net::HTTP.post_form(URI.parse(base + "/collections/" + key + "/ingest"), {"j" => rec_json})
 				if !response.body.empty?
+					j_count = j_count + 1
 					puts "success!"
 				end
-				
-				j_count = j_count + 1
 			end
 		end
 		puts "Processed " + j_count.to_s + " JSON files (out of " + (ent.count - 2).to_s + " total files in directory)"
